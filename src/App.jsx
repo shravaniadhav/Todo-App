0ef362e4e8070d416ebc78eafc5527cff58c1fb4
shravaniadhav <shrauva2007@gmail.com>
2025-11-29 14:55:56 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
 import Todo from "./components/todo";
-import {Provider} from "react-redux";
-import {store} from "./app/store";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { useEffect } from "react";
 
 function App() {
 
+
+  useEffect(() => {
+    document.title = "Todo-App";
+  }, []);
+
   return (
-    <>
     <Provider store={store}>  
       <Todo/>
     </Provider>
-    </>
   );
 }
 
